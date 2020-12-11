1c96834ca2c1a474e37d3567c37589123ab16d98
iammahir2020 <73765820+iammahir2020@users.noreply.github.com>
2020-12-11 22:50:33 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready()
+}
+
+function ready() {
+    
+
+    var addToCartButtons = document.getElementsByClassName('add-to-cart-button')
+    for (var i = 0; i < addToCartButtons.length; i++) {
+        var button = addToCartButtons[i]
+        button.addEventListener('click', addToCartClicked)
+    }
+
+    
+}
+
+
 
 let products=[];
 
@@ -11,14 +30,23 @@ if(products.length>0){
         
         let item_photo = $('<img>').attr("src",products[i].photo);
         let item_name = $('<p>').text("Name: "+products[i].name);
-        let item_description = $('<p>').text(" Description: "+products[i].description);
-        let item_price = $('<p>').text(" Price: "+products[i].price);
-        
-        item_photo.addClass("bla")
+        let item_description = $('<p>').text("Description: "+products[i].description);
+        let item_price = $('<p>').text("Price: "+products[i].price);
         let add_to_cart = $('<button>').text("Add to cart")
-        add_to_cart.addClass("btn")
+        
+        let remove_product = $('<button>').text("Remove Product")
+        
+
+        item_photo.addClass("shop-item-image")
+        item_name.addClass("shop-item-name")
+        item_description.addClass("shop-item-description")
+        item_price.addClass("shop-item-price")
+        add_to_cart.addClass("add-to-cart-button")
+        remove_product.addClass("remove-from-store-button")
+        
+        
         let item_holder = $('<div>')
-        item_holder.append(item_photo, item_name, item_description, item_price,add_to_cart)
+        item_holder.append(item_photo, item_name, item_price,add_to_cart,remove_product)
         
         
         $('#image_holder').append(item_holder) 
@@ -37,17 +65,25 @@ $('#save_button').click(function(event){
         
         let item_photo = $('<img>').attr("src",photo);
         let item_name = $('<p>').text("Name: "+name);
-        let item_description = $('<p>').text(" Description: "+description);
-        let item_price = $('<p>').text(" Price: "+price);
-        
-        item_photo.addClass("bla")
-       
+        let item_description = $('<p>').text("Description: "+description);
+        let item_price = $('<p>').text("Price: "+price);
         let add_to_cart = $('<button>').text("Add to cart")
-        add_to_cart.addClass("btn")
-       let item_holder = $('<div>')
-       item_holder.append(item_photo, item_name, item_description,item_price,add_to_cart)
-       
-       $('#image_holder').append(item_holder) 
+        
+        let remove_product = $('<button>').text("Remove Product")
+        
+
+        item_photo.addClass("shop-item-image")
+        item_name.addClass("shop-item-name")
+        item_description.addClass("shop-item-description")
+        item_price.addClass("shop-item-price")
+        add_to_cart.addClass("add-to-cart-button")
+        remove_product.addClass("remove-from-store-button")
+        
+
+        let item_holder = $('<div>')
+        item_holder.append(item_photo, item_name,item_price,add_to_cart,remove_product)
+     
+        $('#image_holder').append(item_holder) 
         
         
        
@@ -71,3 +107,23 @@ const reset_input_fields=()=>{
     $('#p_price').val("");
     
 } 
+
+
+// function addToCartClicked(event){
+//     var button = event.target
+//     var shopItem = button.parentElement.parentElement
+//     var title = shopItem.getElementsByClassName('shop-item-name')[1].innerText
+//     var price = shopItem.getElementsByClassName('shop-item-price')[1].innerText
+//     var imageSrc = shopItem.getElementsByClassName('shop-item-image')[1].src
+//     alert(title+price+imageSrc)
+    
+// }
+
+const addToCartClicked=()=>{
+    var elem = $(this); //returns inner div
+    
+    var child = elem.find(".shop-item-price").val(); //returns block class
+    alert(elem);
+    console.log(child)
+}
+
